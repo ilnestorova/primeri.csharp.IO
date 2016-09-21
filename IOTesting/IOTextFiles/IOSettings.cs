9b37f42cf2f5a91adf672db7ace4fca6873412f9
ilnestorova <iliana.nestorova@gmail.com>
2016-09-21 15:29:59 +0300

    VModul8 Video3

---
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTextFiles
+{
+    public class IOSettings
+    {
+        private STable nstable;
+        public IOSettings(STable stable)
+        {
+            nstable = stable;
+        }
+        public string GethPath()
+        {
+            string path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+            return path;
+        }
+        // zasdavame bool method za da moje da proverim dali ima zapis
+        // if use only System.IO.File.WriteAllText - nqma da znaem dali naistina e zapisan
+        public bool save()
+        {
+            try
+            {
+                string temp = "";
+                temp = String.Join("\r\n", nstable.stable);
+                //zapis na textov file 
+                //toi se namira v temp
+               // System.IO.File.WriteAllText("c:\\AulaNew\\test.txt", temp);
+                System.IO.File.WriteAllText(GethPath(), temp);
+                return true;
+            }catch { }
+            return false;
+        }
+        public bool open()
+        {
+            try
+            {
+                string temp = System.IO.File.ReadAllText(GethPath());
+                string[] table = temp.Replace("\r", "").Split('\n');
+                for(int i=0; i<table.Length; i++)
+                {
+                    nstable.stable[i] = table[i];
+                }
+                return true;
+            }catch { }
+            return false;
+        }
+    }
+}
+/*
+string file = //четеш го от някаде
+
+string[] row = file.Split ('\n');
+
+for (int i = 0; i < row.Lenght; i++)
+{
+     string[] value = row[i].Split ('\t');
+     value[0] // Ще е стойността от първата колона на iтия ред
+}
+*/
---
+++ IOTesting/IOTextFiles/IOTextFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTextFiles
+{
+    class IOTextFile
+    {
+       
+        static void Main(string[] args)
+        {
+            STable nSTable = new STable();
+            IOSettings nio = new IOSettings(nSTable);
+           // Console.WriteLine(nSTable.stable[0]);
+            // if (nio.save()) WriteLine("The table is saved");
+            if (nio.open()) Console.WriteLine("The table is readed");
+            else Console.WriteLine("The table is NOT readed");
+            Console.WriteLine(nSTable.stable[2]);
+        }
+    }
+}
